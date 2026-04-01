e58ff2ea5d3639bfd0802322bd84ce7be22f18ea
arhamg22 <arham22kk@gmail.com>
2026-04-02 00:17:04 +0530

    Create sum_of_evennumbers.c

---
+++ sum_of_evennumbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter value of N: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        if(i % 2 == 0) {
+            sum += i;
+        }
+    }
+
+    printf("Sum of even numbers = %d\n", sum);
+
+    return 0;
+}
